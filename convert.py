40f8ab24634943d4394a237d74b5eb8eef335763
Fiete <41323592+FieteO@users.noreply.github.com>
2021-08-10 15:08:14 +0200

    Switch to the new csv file

--- convert.py
+++ convert.py
@@ -1,5 +1,6 @@
 import re
 import pandas as pd
+import numpy as np
 from exporter import text_to_textline
 
 """
@@ -9,19 +10,19 @@ that contains the reports split into one sentence per line
 input = "data/captum.csv"
 output = "data/captum.text"
 
-def convert(reports):
-    textline = []
-    for index, row in reports.iterrows():
-        textline.extend(text_to_textline(row['text']))
+# def convert(reports):
+#     textline = []
+#     for index, row in reports.iterrows():
+#         textline.extend(text_to_textline(row['text']))
 
-    with open(output, 'w') as outfile:
-        for e in textline:
-            outfile.write(e+'\n')
+#     with open(output, 'w') as outfile:
+#         for e in textline:
+#             outfile.write(e+'\n')
 
 try:
     reports = pd.read_csv(input)
     print(f"Converting '{input}' into '{output}'...")
-    convert(reports)
+    np.savetxt(output, reports['lemma'].sample(n=9500,random_state=2).values, fmt='%s')
     print(f'Conversion completed.')
 except FileNotFoundError:
     print(f"File '{input}' could not be found.")
